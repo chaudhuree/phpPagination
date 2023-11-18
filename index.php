beb19ee959090301ac42f105aeb5400c82dea862
chaudhuree <chaudhuree00@gmail.com>
2023-11-19 01:13:06 +0600

    pagination completed

--- index.php
+++ index.php
@@ -8,6 +8,7 @@ include 'script.php';
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <title>php mysqli pagination</title>
+  <link rel="stylesheet" href="style.css">
 </head>
 
 <body>
@@ -19,6 +20,66 @@ include 'script.php';
     <?php
     }
     ?>
+  </div>
+  <!-- Display the page info text -->
+  <div class="page-info">
+    <?php
+    if (!isset($_GET['page-no'])) {
+      $page = 1;
+    } else {
+      $page = $_GET['page-no'];
+    }
+    ?>
+    Showing <?php echo $page ?> of <?php echo $pages ?> Pages
+
+  </div>
+
+  <!-- Display the pagination button -->
+  <div class="pagination">
+    <!-- go to the first page -->
+    <a href="?page-no=1">First</a>
+    <!-- go to the previous page -->
+    <?php
+
+    if (isset($_GET['page-no']) && $_GET['page-no'] > 1) {
+    ?>
+      <a href="?page-no=<?php echo $_GET['page-no'] - 1 ?>">Previous</a>
+    <?php
+    } else {
+    ?>
+      <a disabled class="disabled-page">Previous</a>
+    <?php
+    }
+    ?>
+    <!-- output the page numbers -->
+    <div class="page-numbers">
+      <?php
+      for ($counter = 1; $counter <= $pages; $counter++) {
+      ?>
+        <a href="?page-no=<?php echo $counter ?>"><?php echo $counter ?></a>
+      <?php
+      }
+      ?>
+    </div>
+
+    <!-- go to next page -->
+    <?php
+    if (isset($_GET['page-no']) && $_GET['page-no'] < $pages) {
+    ?>
+      <a href="?page-no=<?php echo $_GET['page-no'] + 1 ?>">Next</a>
+    <?php
+    } else {
+    ?>
+      <a class="disabled-page" disabled>Next</a>
+    <?php
+    }
+    ?>
+
+
+    <!-- go to the last page -->
+    <a href="?page-no=<?php echo $pages ?>">Last</a>
+  </div>
+
 </body>
 
 </html>
--- script.php
+++ script.php
@@ -8,4 +8,20 @@
       $mysqli->set_charset("utf8mb4");	
    }
 
-   $result=$mysqli->query("SELECT * FROM `products` LIMIT 0,4");
+   $start=0;
+   $rows_per_page=4;
+
+  //  get the total of rows
+  $records=$mysqli->query("SELECT * FROM `products`");
+  $number_of_rows = $records->num_rows;
+
+  // calculate the number of pages we are going to have
+  $pages = ceil($number_of_rows/$rows_per_page);
+
+   if(isset($_GET['page-no'])){
+    $page=$_GET['page-no']-1;
+    $start=$rows_per_page*$page;
+   }
+
+
+   $result=$mysqli->query("SELECT * FROM `products` LIMIT $start, $rows_per_page");
